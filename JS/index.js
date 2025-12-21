987111dc6ff7445c77fdd72d442830c001785279
Riddhi <riddhcodes@gmail.com>
2025-12-21 18:31:50 +0530

    Add restart button to footer

--- JS/index.js
+++ JS/index.js
@@ -18,6 +18,7 @@ const restartButtonEl = document.getElementById("restart-button");
 const currentTimeEl = document.getElementById("current-time");
 const bestTimeEl = document.getElementById("best-time");
 const finalTimeEl = document.getElementById("final-time");
+const uiRestartBtn = document.getElementById("ui-restart-button");
 
 const playerState = {
   x: 0,
@@ -288,6 +289,11 @@ function bootstrap() {
   document.addEventListener("keydown", handleKeyDown);
   document.addEventListener("keyup", handleKeyUp);
   restartButtonEl.addEventListener("click", () => startGame());
+
+  uiRestartBtn.addEventListener("click", () => {
+    startGame();
+    uiRestartBtn.blur();
+  });
 
   gameState.bestTime = loadBestTime();
   centerPlayer();
